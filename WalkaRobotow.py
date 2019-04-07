32b53bca713c1a6bf785286193fe1613862547a3
Kamil Gierlach <kamil@MacBook-Air-Kamil-2.local>
2019-04-07 17:39:53 +0200

    modified:   WalkaRobotow.py

--- WalkaRobotow.py
+++ WalkaRobotow.py
@@ -1,7 +1,8 @@
 #       Game
 #   Robocop
 #   By Kamil Gierlach
-#   20 - 02 - 2019
+#   start date: 20 - 02 - 2019
+#   last update: 07 - 04 - 2019
 # -*- coding: utf-8 -*-
 from tkinter import *
 import tkinter as tk
@@ -19,6 +20,7 @@ height = 400 / 20
 size = 20
 speed = 100
 score = 0
+rd = 0 #round
 
 def game():
     window.destroy()
@@ -28,46 +30,16 @@ def game():
     window2.geometry('600x450+200+200')
 
     text1 = Label(window2, text = 'Player', bg = 'Light Blue')
-    text1.grid(column = 0, row = 0, padx = 25)
+    text1.grid(column = 0, row = 0)
 
     text2 = Label(window2, text = '  Computer', bg = 'Light Blue')
-    text2.grid(column = 3, row = 0)
-
-    #score receive by player
-    def Score():
-        scr['text'] = 'Score:', str(score)
-        window2.after(1000, Score)
-
-    scr = Label(window2, text = '00', bg = 'Light Blue')
-    scr.grid(column = 0, row = 1)
-    Score()
-
-    # def update_time():
-    #     start = time.localtime()
-    #     sec = start.tm_sec - koniec.tm_sec
-    #     min = start.tm_min - koniec.tm_min
-    #
-    #     if min < 10:
-    #         if sec < 10:
-    #             lbl['text'] = ('0', min, ":", '0', sec)
-    #         else:
-    #             lbl['text'] = ('0', min, ":", sec)
-    #     else:
-    #         lbl['text'] = min, ':', sec
-    #
-    #     window2.after(1000, update_time)
-    #
-    # koniec = time.localtime()
-    # lbl = Label(window2, text = '00:00', bg = 'Light Blue')
-    # lbl.grid(column = 1, row = 0)
-    # update_time()
+    text2.grid(column = 6, row = 0)
 
     canvas = tk.Canvas(window2, width='400', height='400')
     canvas.configure(bg='light blue')
-    canvas.grid(column = 1, row = 1)
-    # canvas.pack()
+    canvas.grid(column = 1, columnspan = 4 , row = 1, rowspan = 10)
 
-    # make area
+    # create area
     class area():
         def __init__(self):
             self.width = int(width)
@@ -85,30 +57,91 @@ def game():
             canvas.create_rectangle(380, 0, 400, 20, fill='light yellow')
             canvas.create_rectangle(0, 380, 20, 400, fill='light yellow')
 
-    # make robot
+    # create robot
     class robot():
-        def __init__(self, hp, x, y, color, player):
+        def __init__(self, hp, x, y, color, visible_player):
             self.width = int(width)
             self.height = int(height)
             self.verctor_move = [1, 0]  # initial vector
             self.body = [x, y]  # initial cords of body
             self.size = size
             self.hp = hp
-            self.Robot = False
-            self.Player = bool(player)
+            self.Robot = False #visible robot on map
+            self.visible_Player = bool(visible_player) #robot players or computer
             self.color = color
+            self.damage = 0
+            self.killed_enemies = 0
 
+        def Statistic(self):
+            self.hp_text = Label(window2, text='HP: ', bg='light blue')
+            self.damage_text = Label(window2, text=('Damage:', str(self.damage)), bg='Light blue')
+            if self.visible_Player == True:
+                self.hp_text.configure(text = ('HP:', str(self.hp)))
+                self.hp_text.grid(column=0, row=1)
+                self.damage_text.grid(column=0, row=2)
+            elif self.visible_Player == False:
+                self.hp_text.configure(text = ('HP:', str(self.hp)))
+                self.hp_text.grid(column=6, row=1)
+                self.damage_text.grid(column=6, row=2)
+
+        #create stopwatch
+        def update_time(self):
+            self.DTime = Label(window2, text='00', bg='Light blue')
+            self.DTime.grid(column=3, row=0)
+            koniec = time.localtime()
+            sec = koniec.tm_sec - self.start.tm_sec
+            min = koniec.tm_min - self.start.tm_min
+            if min < 10:
+                if sec < 10:
+                    Time = '0', min, ':', '0', sec
+                else:
+                    Time = '0', min, ':', sec
+            else:
+                Time = min, ':', sec
+            self.DTime['text'] = Time
+            window2.after(1000, self.update_time)
+
+        #round of the game - it changes when 5 enemies die
+        def rd(self):
+            global rd
+            text3 = Label(window2, text=('Round:', rd), bg='light blue')
+            text3.grid(column=2, columnspan=1, row=0)
+            if self.killed_enemies % 5 == 0 and self.killed_enemies > 0:
+                rd += 1
+                text3['text'] = 'Round:', rd
+            elif self.killed_enemies >= 0 and self.killed_enemies % 5 != 0:
+                print('else')
+                window2.after(1000, self.rd)
+
+        #score receive by player
+        def Score(self):
+            self.scr = Label(window2, text=('Score:', score), bg='Light Blue')
+            self.scr.grid(column=0, row=3)
+            self.scr['text'] = 'Score:', str(score)
+            window2.after(1000, self.Score)
+
+        def draw_Stat(self):
+            self.start = time.localtime()
+            self.update_time()
+            self.Statistic()
+            self.Score()
+            self.rd()
 
         def Attack(self):
             self.hp -= 10
             if self.hp > 0:
                 canvas.delete(self.Hp)
                 self.Hp = canvas.create_text((self.body[0] + 10, self.body[1] + 10), text=self.hp)
-            elif self.hp <= 0 and self.Player == False:
+            elif self.hp <= 0 and self.visible_Player == False:
                 canvas.delete(self.robot, self.Hp)
                 self.new_robot(100, (rn.randint(1, 19) * size), (rn.randint(1, 19) * size))
+                self.killed_enemies += 1
             elif self.hp <= 0:
                 canvas.delete(self.robot, self.Hp)
+                canvas.delete('all')
+                end = Label(window2, text = 'Game Over', bg = 'light blue')
+                end.grid(column = 1, columnspan = 4, row = 1, rowspan = 10)
+            print(self.killed_enemies)
 
 
         def drawRobot(self):
@@ -123,25 +156,24 @@ def game():
                     self.robot = canvas.create_rectangle(self.body[0], self.body[1], self.body[0] + size,
                                                          self.body[1] + size, fill=self.color)
                     self.Hp = canvas.create_text((self.body[0] + 10, self.body[1] + 10), text=self.hp)
-            elif self.hp < 0 and self.Player == False:
+            elif self.hp < 0 and self.visible_Player == False:
                 canvas.delete(self.robot, self.Hp)
                 self.body = [400, 400]
             elif self.hp < 0:
                 canvas.delete(self.robot, self.Hp)
-
 
         def move(self):
             self.body[0] = self.body[0] + (self.verctor_move[0] * size)
             self.body[1] = self.body[1] + (self.verctor_move[1] * size)
             self.drawRobot()
 
+        #create new robot when copmuter die
         def new_robot(self, hp, x, y):
             if self.hp <= 0:
                 self.Robot = False
                 self.hp = hp
                 self.body = [x, y]
                 self.drawRobot()
-
         # change initial vector
         def move_up(self):
             if self.body[1] > 0:
@@ -173,7 +205,8 @@ def game():
         # computer Robocop - ROBIT
         ROBPL = robot(100, 0, 0, 'Blue', True)
         ROBIT = robot(100, (rn.randint(1, 19) * size), (rn.randint(1, 19) * size), 'Red', False)
-
+        ROBPL.draw_Stat()
+        ROBIT.draw_Stat()
 
         def move():
             ROBPL.move()
@@ -187,8 +220,6 @@ def game():
                 ROBIT.Attack()
                 global score
                 score += 10
-                ROBPL.drawRobot()
-
 
         def moveLeft(event):
             if ROBPL.body[0] != ((ROBIT.body[0] + 20)) or ROBPL.body[1] != ROBIT.body[1]:
@@ -198,7 +229,6 @@ def game():
                 ROBIT.Attack()
                 global score
                 score = score + 10
-                ROBPL.drawRobot()
 
         def moveUp(event):
             if ROBPL.body[0] != ROBIT.body[0] or ROBPL.body[1] != (ROBIT.body[1] + 20):
@@ -208,7 +238,6 @@ def game():
                 ROBIT.Attack()
                 global score
                 score = score + 10
-                ROBPL.drawRobot()
 
         def moveDown(event):
             if ROBPL.body[0] != ROBIT.body[0] or ROBPL.body[1] != (ROBIT.body[1] - 20):
@@ -218,7 +247,7 @@ def game():
                 ROBIT.Attack()
                 global score
                 score = score + 10
-                ROBPL.drawRobot()
+
 
         # set keybord
         window2.after(speed, move)
@@ -226,6 +255,7 @@ def game():
         window2.bind_all("<KeyPress-Right>", moveRight)
         window2.bind_all("<KeyPress-Up>", moveUp)
         window2.bind_all("<KeyPress-Down>", moveDown)
+
 
     if __name__ == '__main__':
         Game()
@@ -244,11 +274,9 @@ def menu():
 
 menu()
 
-#wypisywanie punktow na ekranie
 #sztuczna inteligencja
 #robot pojawia sie i porusza w kierunku gracza
 #lvl zwieksza sie poziom computera
-#czas
 #rundy - konczy sie po zabiciu 5 przeciwnikow
 #po 3 rundach jest boss 3Xstaty zwyklego IT
 
